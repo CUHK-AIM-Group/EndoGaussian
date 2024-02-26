9031a62098ec038f61888f9b7f21eaa8297a3586
yifliu3 <1155195605@link.cuhk.edu.hk>
2024-02-27 00:41:58 +0800

    opacity_reset

--- arguments/endonerf/pulling.py
+++ arguments/endonerf/pulling.py
@@ -10,9 +10,9 @@ OptimizationParams = dict(
     deformation_lr_delay_mult = 0.01,
     grid_lr_init = 0.0016,
     grid_lr_final = 0.000016,
-    iterations = 3000,
+    iterations = 4000,
     percent_dense = 0.01,
-    opacity_reset_interval = 3000,
+    opacity_reset_interval = 4000,
     position_lr_max_steps = 4000,
     prune_interval = 3000
 )
@@ -21,8 +21,8 @@ ModelHiddenParams = dict(
     kplanes_config = {
      'grid_dimensions': 2,
      'input_coordinate_dim': 4,
-     'output_coordinate_dim': 32,
-     'resolution': [32, 32, 32, 50]
+     'output_coordinate_dim': 64,
+     'resolution': [64, 64, 64, 100]
 
     },
     multires = [1, 2, 4, 8],
